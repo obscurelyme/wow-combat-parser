22420bb8f40803ab5a27d3ce206147655d0217ed
Mackenzie Greco <nico.frozencoffee@gmail.com>
2023-11-24 23:49:23 -0800

    more encounter details

--- packages/obscure-ui/src/Routes/Encounter/index.tsx
+++ packages/obscure-ui/src/Routes/Encounter/index.tsx
@@ -1,8 +1,65 @@
-import { Box, Divider, Typography } from '@mui/material';
+import { useState } from 'react';
+
+import { Box, Divider, Typography, List, ListItem, ListItemButton, ListItemText, Collapse } from '@mui/material';
 
 import { useEncounterLoaderData } from './loader';
 import JournalEncounterLootTable from '../../Composites/JournalEncounterLootTable';
 import PageHeader from '../../Composites/PageHeader';
+import { BnetCommon } from '@obscure/types/dist';
+
+interface EncounterPhaseProps {
+  phase: BnetCommon.Phase;
+}
+
+function EncounterPhase({ phase }: EncounterPhaseProps): React.ReactElement {
+  const [open, setOpen] = useState<boolean>(false);
+
+  function handleClick() {
+    setOpen(!open);
+  }
+
+  return (
+    <>
+      <ListItemButton key={`phase-${phase.id}`} divider onClick={handleClick}>
+        <ListItemText primary={phase.title} secondary={phase.body_text}></ListItemText>
+      </ListItemButton>
+      <Collapse in={open} unmountOnExit timeout="auto">
+        <List sx={{ pl: 4 }}>
+          {phase.sections?.map(phaseSection => {
+            if (phase.title === 'Overview') {
+              const overviewSections = phaseSection.body_text.split('$bullet;');
+              overviewSections.shift();
+
+              return (
+                <ListItem key={`phase-section-${phaseSection.id}`}>
+                  <ListItemText
+                    primary={phaseSection.title}
+                    secondary={
+                      <>
+                        {overviewSections.map(s => {
+                          return (
+                            <Box>
+                              <Typography key={`s-${s}`}>{s}</Typography>
+                            </Box>
+                          );
+                        })}
+                      </>
+                    }
+                  />
+                </ListItem>
+              );
+            }
+            return (
+              <ListItem key={`phase-section-${phaseSection.id}`}>
+                <ListItemText primary={phaseSection.title} secondary={phaseSection.body_text} />
+              </ListItem>
+            );
+          })}
+        </List>
+      </Collapse>
+    </>
+  );
+}
 
 export function EncounterPage() {
   const { journalEncounter, bnet } = useEncounterLoaderData();
@@ -15,9 +72,17 @@ export function EncounterPage() {
 
       <Divider />
 
-      <Typography variant="body2">{journalEncounter?.description}</Typography>
+      <Typography maxWidth="700px" variant="body2">
+        {journalEncounter?.description}
+      </Typography>
 
       <Divider />
+
+      <List disablePadding>
+        {bnet?.encounterData.sections?.map(phase => {
+          return <EncounterPhase key={`phase-${phase.id}`} phase={phase} />;
+        })}
+      </List>
 
       {bnet && <JournalEncounterLootTable drops={bnet?.encounterData.items} />}
     </Box>
